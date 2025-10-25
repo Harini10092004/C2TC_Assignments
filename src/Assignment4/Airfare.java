897fa381229a6c143627aa08b06c97d558340557
harin <harin@Harini_Ramesh>
2025-10-25 05:51:27 +0530

    commit

---
+++ src/Assignment4/Airfare.java
@@ -0,0 +1,10 @@
+package Assignment4;
+
+//Airfare.java
+//Interface declaring methods to be implemented by airline classes
+
+public interface Airfare {
+ Double calculateAmount();
+ void display();
+}
+
