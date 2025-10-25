0b3fda2751a8e868ac6be192e9667df86bad392c
harin <harin@Harini_Ramesh>
2025-10-25 05:45:26 +0530

    commit

---
+++ src/Assignment3/CheckingAccount.java
@@ -0,0 +1,33 @@
+package Assignment3;
+
+//CheckingAccount.java
+//Represents a Checking Account extending Account
+
+public class CheckingAccount extends Account {
+
+ public CheckingAccount(String accountHolder, double initialBalance) {
+     super(accountHolder, initialBalance);
+ }
+
+ @Override
+ public void deposit(double amount) {
+     balance += amount;
+     System.out.println("Deposited ₹" + amount + ". New Balance: ₹" + balance);
+ }
+
+ @Override
+ public void withdraw(double amount) {
+     if (amount <= balance) {
+         balance -= amount;
+         System.out.println("Withdrawn ₹" + amount + ". Remaining Balance: ₹" + balance);
+     } else {
+         System.out.println("Overdraft not allowed! Withdrawal denied.");
+     }
+ }
+
+ @Override
+ public double getBalance() {
+     return balance;
+ }
+}
+
